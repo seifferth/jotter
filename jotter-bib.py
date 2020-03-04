2c04844e84d0cdba21895fa683cb5624db8735ff
Frank Seifferth <frankseifferth@posteo.net>
2020-03-04 14:08:45 +0000

    Make dependency resolution optional

--- jotter-bib.py
+++ jotter-bib.py
@@ -9,6 +9,8 @@ Positional Arguments:
 Options:
     -l  --list      List all bibtex citekeys defined in this jotter
     -L  --list-all  List all citekeys, including non-bibtex ones
+        --no-deps   Do not include dependencies for "crossref" or
+                    "xref" entries
     -h  --help      Print this help message and exit
 """
 
@@ -16,7 +18,7 @@ import sys
 import re
 from jotter import find_jotter_root, survey
 
-def get_bib(citekey_map, citekeys: list):
+def get_bib(citekey_map, citekeys: list, deps=True):
     done = set()
     for key in citekeys:
         if key in done:
@@ -25,13 +27,15 @@ def get_bib(citekey_map, citekeys: list):
             bibtex = re.split(r'\n\s*@', "\n"+citekey_map[key]["bibtex"])
             for entry in bibtex:
                 if re.match(r'^.*?{'+key+r',', entry):
-                    crossref = re.findall(
-                        r'(xref|crossref)\s*=\s*[{"](.*?)[}"]',
-                        entry,
-                        re.I,
-                    )
-                    for crossref_type, crossref_key in crossref:
-                        if crossref_key not in done:
+                    if deps:
+                        crossref = re.findall(
+                            r'(xref|crossref)\s*=\s*[{"](.*?)[}"]',
+                            entry,
+                            re.I,
+                        )
+                        for crossref_type, crossref_key in crossref:
+                            if crossref_key in done:
+                                continue
                             for dep in get_bib(citekey_map, [crossref_key]):
                                 dep_key = re.findall(r'^@.*?{(.*?),', dep)[0]
                                 if dep_key not in done:
@@ -56,12 +60,12 @@ def get_citekey_map():
     _, citekey_map, _ = survey(find_jotter_root(), full_content=False)
     return citekey_map
 
-def print_bib(citekeys):
+def print_bib(citekeys, deps=True):
     citekey_map = get_citekey_map()
     if not citekeys:
         citekeys = get_citekeys(citekey_map)
     first = True
-    for entry in get_bib(citekey_map, citekeys):
+    for entry in get_bib(citekey_map, citekeys, deps=deps):
         if first:
             first = False
         else:
@@ -73,8 +77,12 @@ def print_citekeys(real=True):
 
 
 if __name__ == "__main__":
+    deps = True
+    if "--no-deps" in sys.argv:
+        deps=False
+        sys.argv.remove("--no-deps")
     if len(sys.argv) == 1:
-        print_bib(None)
+        print_bib(None, deps=deps)
     elif "-h" in sys.argv or "--help" in sys.argv:
         print(__doc__); exit(0)
     elif sys.argv[1] in ["-l", "--list"]:
@@ -82,4 +90,4 @@ if __name__ == "__main__":
     elif sys.argv[1] in ["-L", "--list-all"]:
         print_citekeys(real=False)
     else:
-        print_bib(sys.argv[1:])
+        print_bib(sys.argv[1:], deps=deps)
