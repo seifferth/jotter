5a4c35725ee8295151d45416f39592d4fdd0bc73
Frank Seifferth <frankseifferth@posteo.net>
2020-03-04 13:18:34 +0000

    Add further command line options to jotter-bib

--- jotter-bib.py
+++ jotter-bib.py
@@ -1,24 +1,69 @@
 #!/usr/bin/env python3
-"""Usage: jotter-bib [-h|--help]
+"""Usage: jotter-bib [options] [citekeys]..
 
-Extract all bibtex fields from markdown files
+Extract bibtex fields from markdown files
+
+Positional Arguments:
+    citekeys        Only include specific citekeys. If this argument
+                    is omitted, the full bibliography will be printed.
+Options:
+    -l  --list      List all bibtex citekeys defined in this jotter
+    -L  --list-all  List all citekeys, including non-bibtex ones
+    -h  --help      Print this help message and exit
 """
 
 import sys
+import re
 from jotter import find_jotter_root, survey
 
-if __name__ == "__main__":
-    if "-h" in sys.argv or "--help" in sys.argv:
-        print(__doc__); exit(0)
-    else:
-        filename_map, _, _ = survey(find_jotter_root(), full_content=False)
-        bibtex = map(
-            lambda doc: doc["bibtex"],
-            filter(
-                lambda doc: "bibtex" in doc.keys(),
-                filename_map.values()
-            )
+def get_bib(citekey_map, citekeys: list):
+    for key in citekeys:
+        try:
+            bibtex = re.split(r'\n\s*@', "\n"+citekey_map[key]["bibtex"])
+            for entry in bibtex:
+                if re.match(r'^.*?{'+key+r',', entry):
+                    yield '@'+re.sub(r'\n+$', '', entry)
+                    break
+        except KeyError:
+            print("No bibtex entry for {}".format(key), file=sys.stderr)
+
+def get_citekeys(citekey_map, real=True):
+    if real:
+        return filter(
+            lambda key: "bibtex" in citekey_map[key],
+            citekey_map.keys()
         )
-        for entry in bibtex:
-            print(entry)
-            print() # Just to make sure
+    else:
+        return citekey_map.keys()
+
+def get_citekey_map():
+    _, citekey_map, _ = survey(find_jotter_root(), full_content=False)
+    return citekey_map
+
+def print_bib(citekeys):
+    citekey_map = get_citekey_map()
+    if not citekeys:
+        citekeys = get_citekeys(citekey_map)
+    first = True
+    for entry in get_bib(citekey_map, citekeys):
+        if first:
+            first = False
+        else:
+            print()         # Separate entries by newline
+        print(entry)
+
+def print_citekeys(real=True):
+    print("\n".join(get_citekeys(get_citekey_map(), real=real)))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print_bib(None)
+    elif "-h" in sys.argv or "--help" in sys.argv:
+        print(__doc__); exit(0)
+    elif sys.argv[1] in ["-l", "--list"]:
+        print_citekeys(real=True)
+    elif sys.argv[1] in ["-L", "--list-all"]:
+        print_citekeys(real=False)
+    else:
+        print_bib(sys.argv[1:])
